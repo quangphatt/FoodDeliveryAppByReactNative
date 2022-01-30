c4ba7b4e9095bcb57403b5d0f1fa362c585e26bd
Quang Phat <phatquang2811@gmail.com>
2022-01-30 23:28:17 +0700

    add bottom tab navigation

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 
-import {Home, Restaurant, OrderDelivery} from './screens'
+import {Home, Restaurant, OrderDelivery} from './screens';
+import Tabs from './navigation/tabs';
 
-import { StyleSheet, Text, View, LogBox } from 'react-native';
+import { Text, View, LogBox } from 'react-native';
 
 const Stack=createStackNavigator();
 
@@ -21,7 +22,7 @@ const App = () => {
 					}}
 				initialRouteName={'Home'}
 			>
-				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Home" component={Tabs} />
 				<Stack.Screen name="Restaurant" component={Restaurant} />
 				<Stack.Screen name="OrderDelivery" component={OrderDelivery} />
 			</Stack.Navigator>
--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -2,11 +2,11 @@ import { View, Text } from 'react-native';
 import React from 'react';
 
 const OrderDelivery = () => {
-  return (
-    <View>
-      <Text>OrderDelivery</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text>OrderDelivery</Text>
+		</View>
+	);
 };
 
 export default OrderDelivery;
